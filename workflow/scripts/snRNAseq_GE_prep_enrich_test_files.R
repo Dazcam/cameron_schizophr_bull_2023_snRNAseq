c23e45d783638257bd20350d89dd69f47073ce84
Dazcam <camerond@cardiff.ac.uk>
2023-04-17 11:03:32 +0100

    Fix order of MAGMA ref file

--- workflow/scripts/snRNAseq_GE_prep_enrich_test_files.R
+++ workflow/scripts/snRNAseq_GE_prep_enrich_test_files.R
@@ -40,12 +40,12 @@ cat('\n\nMHC genes:', length(mhc_genes_uniq), '\n')
 # Remove MHC gene from gene coordinate reference file (and write to file)  ------------
 gene_coordinates <- read_tsv(paste0(DATA_DIR, 'refs/NCBI37.3.gene.loc.txt'),
                              col_names = FALSE, col_types = 'cciicc') %>%
-  dplyr::rename(chr = "X2", entrez = "X1", chr = "X2", 
-                start = 'X3', end = 'X4', hgnc = 'X6') %>%
-  dplyr::select(chr, start, end, entrez, hgnc) %>% 
+  dplyr::rename(entrez = "X1", chr = "X2", start = 'X3', 
+                end = 'X4', strand = "X5", hgnc = 'X6') %>%
   filter(!hgnc %in% mhc_genes_uniq) %>%
   write_tsv(paste0(DATA_DIR, 'refs/NCBI37.3.MHCremoved.gene.loc.txt'), col_names = FALSE) %>%
-  mutate(chr = paste0("chr", chr))
+  mutate(chr = paste0("chr", chr)) %>%
+  dplyr::select(chr, start, end, entrez, hgnc) 
 
 
 ##  Create ctd object  ----------------------------------------------------------------
