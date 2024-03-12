f9583f77b6acfe586636dfeb43efdf92fa451e2a
Dazcam <camerond@cardiff.ac.uk>
2024-03-12 15:15:00 +0000

    Rm shadowtext test code

--- workflow/scripts/snRNAseq_GE_plots_for_paper.R
+++ workflow/scripts/snRNAseq_GE_plots_for_paper.R
@@ -155,8 +155,8 @@ cnts <- data.frame(Region_cell_type  = c("Ganglionic eminence", "CGE-N", "LGE-N"
 )
 
 
-# Final paper plots
-# Figure 1 - Level 1 cluster UMAP and gene list
+## FINAL PLOTS FOR RNA SEQ PAPER (EXCLUDING BAR PLOTS)  ---------------------
+# Fig 1 - L1 cluster UMAP and Vln  -----
 seurat.shi.bc$cluster_level_1_new <- factor(seurat.shi.bc$cluster_level_1_new,
                                             level=c('LGE-N', 'MGE-N', 'CGE-N',
                                                     'IPC', 'Microglia', 'Progenitor'))
@@ -164,9 +164,7 @@ figure_1A <- DimPlot_scCustom(seurat.shi.bc, group.by = 'cluster_level_1_new',
                               DiscretePalette_scCustomize(num_colors = 26, palette = "ditto_seq"),
                               label = TRUE, repel = TRUE) +
   theme(legend.position = "none") +
-  ggtitle("") + annotate("shadowText", label="Fun", x= labels$x, y=labels$x)
-
-geom_shadowtext(label = cluster_level_1_new, bg.colour='white')
+  ggtitle("") 
 
 figure_1B <- VlnPlot(seurat.shi.bc, LEVEL_1_MARKERS, stack = TRUE, flip = TRUE,
                     cols = DiscretePalette_scCustomize(num_colors = 26, palette = "ditto_seq"),
@@ -175,29 +173,7 @@ figure_1B <- VlnPlot(seurat.shi.bc, LEVEL_1_MARKERS, stack = TRUE, flip = TRUE,
 
 plot_grid(figure_1A, figure_1B, labels = 'AUTO', label_size = 20)
 
-bgWidth = 1
-labelSize = 4
-bgColor = 'white'
-theta <- seq(pi / 8, 2 * pi, length.out = 16)
-xo <- bgWidth * diff(range(figure_1A$data$UMAP_1)) / 300
-yo <- bgWidth * diff(range(figure_1A$data$UMAP_2)) / 300
-for (i in theta) {
-  p <- figure_1A + 
-    geom_text(aes(
-                x = bquote(figure_1A$data$UMAP_1 + .(cos(i) * xo)),
-                y = bquote(figure_1A$data$UMAP_2 + .(sin(i) * yo)),
-                label = ~text
-              ),
-              size = labelSize,
-              color = bgColor
-    )
-  
-}
-
-
-
-
-# Figure 3 - Level 2 cluster UMAPs and violin plots
+# Fig S5-S9 - L2 cluster UMAPs and Vlns -----
 for (SUFFIX in c('_CGE', '_LGE', '_MGE', '_Progenitor', '_Early_InN')) {
 
   cat('\nRunning seurat.shi.bc', SUFFIX, sep = '')
@@ -222,36 +198,25 @@ for (SUFFIX in c('_CGE', '_LGE', '_MGE', '_Progenitor', '_Early_InN')) {
                    label = TRUE) + # Need repel for Prog only otherwise Prog-10 label is cut in image
     theme(legend.position = "none") +
     ggtitle("")
-  
-  assign(paste0('cluster', SUFFIX, '_umap'), UMAP_PLOT, .GlobalEnv)
 
   VLN_PLOT <- VlnPlot(SEURAT_OBJ, MARKERS, stack = TRUE, flip = TRUE,
                        cols = DiscretePalette_scCustomize(num_colors = 26, palette = "ditto_seq"),
                        same.y.lims = TRUE, fill.by = 'ident', group.by = 'cluster_level_2_new') +
     theme(axis.title.x=element_blank(), legend.position = "none")
   
+  assign(paste0('cluster', SUFFIX, '_umap'), UMAP_PLOT, .GlobalEnv)
   assign(paste0('cluster', SUFFIX, '_vln'), VLN_PLOT, .GlobalEnv)
   
 
 }
 
-# Supplementary level 2 figures
-plot_grid(cluster_LGE_umap, cluster_LGE_vln, labels = 'AUTO', label_size = 20)
-plot_grid(cluster_MGE_umap, cluster_MGE_vln, labels = 'AUTO', label_size = 20)
-plot_grid(cluster_CGE_umap, cluster_CGE_vln, labels = 'AUTO', label_size = 20)
-plot_grid(cluster_Progenitor_umap, cluster_Progenitor_vln, labels = 'AUTO', label_size = 20)
-plot_grid(cluster_Early_InN_umap, cluster_Early_InN_vln, labels = 'AUTO', label_size = 20)
+# Fig S5 to S9
+fig_S5 <- plot_grid(cluster_LGE_umap, cluster_LGE_vln, labels = 'AUTO', label_size = 20)
+fig_S6 <-plot_grid(cluster_MGE_umap, cluster_MGE_vln, labels = 'AUTO', label_size = 20)
+fig_S7 <-plot_grid(cluster_CGE_umap, cluster_CGE_vln, labels = 'AUTO', label_size = 20)
+fig_S8 <-plot_grid(cluster_Progenitor_umap, cluster_Progenitor_vln, labels = 'AUTO', label_size = 20)
+fig_S9 <-plot_grid(cluster_Early_InN_umap, cluster_Early_InN_vln, labels = 'AUTO', label_size = 20)
 
-# plot_grid(cluster_Progenitor_umap, cluster_LGE_umap, cluster_MGE_umap,
-#           cluster_CGE_umap, labels = 'AUTO', label_size = 20)
-
-
-# For testing
-# VlnPlot(seurat.shi.bc_MGE, c('SST', 'VIP', 'PVALB', 'NPY', 'NOS', 'NPY', 'CHAT', 'ACHE',
-#                              'TAC1', 'ACLY', 'BRCA1', 'SLC5A7', 'SLC18A3'), stack = TRUE, flip = TRUE,
-#         cols = DiscretePalette_scCustomize(num_colors = 26, palette = "ditto_seq"),
-#         same.y.lims = TRUE, fill.by = 'ident', group.by = 'cluster_level_2_new') +
-#   theme(axis.title.x=element_blank(), legend.position = "none")
 
 
 
